abb0255e28287df275810e4bb059b1dd254170db
Lucas Oliveira Santana da Cunha <lukkzmaverick@gmail.com>
2020-06-19 11:51:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -116,14 +116,11 @@ function cardClick(idCarta) {
 function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
 
-  // While there remain elements to shuffle...
   while (0 !== currentIndex) {
 
-    // Pick a remaining element...
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
 
-    // And swap it with the current element.
     temporaryValue = array[currentIndex];
     array[currentIndex] = array[randomIndex];
     array[randomIndex] = temporaryValue;
